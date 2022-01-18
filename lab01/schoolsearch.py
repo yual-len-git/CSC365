ebf39cee4b4732c967af4ac170dc32a198990867
yual-len <allenyu90@gmail.com>
2022-01-18 00:42:16 -0800

    'first'

---
+++ lab01/schoolsearch.py
@@ -0,0 +1,14 @@
+class students:
+    def __init__(self, stLast, stFirst, grade, room, bus, gpa, tLast, tFirst):
+        self.stLast = stLast
+        self.stFirst = stFirst
+        self.grade = grade
+        self.room = room
+        self.bus = bus
+        self.gpa = gpa
+        self.tLast = tLast
+        self.tFirst = tFirst
+
+
+list = []
+

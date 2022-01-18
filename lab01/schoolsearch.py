c47975cee1fd40c37a81a4b71a8436ea78332b2a
yual-len <allenyu90@gmail.com>
2022-01-18 11:49:28 -0800

    changes to functions

--- lab01/schoolsearch.py
+++ lab01/schoolsearch.py
@@ -19,18 +19,39 @@ def getData(sList):
         sList.append(students(line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7]))
     return sList
 
+def home():
+    print("Enter S[tudent] to search by student last name.")
+    print("Enter T[eacher] to search by teacher last name.")
+    print("Enter G[rade] to search by grade number.")
+    print("Enter B[us] to search by bus route number.")
+    print("Enter A[verage] to search for the average GPA of the grade.")
+    print("Enter I[nfo] for school student info.")
+    print("Enter Q[uit] to end program.")
 
+def searchLastName(sList, last):
+    for student in sList:
+        if student.stLast == last:
+            print(student.stLast + " " + student.stFirst + " " + student.grade + " " + student.room + " " + student.tLast + " " + student.tFirst)
 
+def info(sList):
+    count = 0
+    for student in sList:
+        count+=1
 
+    print()
 
 
 def main():
     studentList = []
     getData(studentList)
 
-    for student in studentList:
-        if student.tFirst == "PERLA":
-            print(student.stFirst + " " + student.stLast)
+    # for student in studentList:
+    #     if student.tFirst == "PERLA":
+    #         print(student.stFirst + " " + student.stLast)
+
+    # while True:
+    home()
+    # searchLastName(studentList, "LINHART")
 
 
 

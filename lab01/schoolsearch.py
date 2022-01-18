45c474228d75254e5751ee5b7af9cd7f7f0cdd4b
yual-len <allenyu90@gmail.com>
2022-01-18 04:08:03 -0800

    reads

--- lab01/schoolsearch.py
+++ lab01/schoolsearch.py
@@ -1,14 +1,38 @@
+import sys
+
 class students:
     def __init__(self, stLast, stFirst, grade, room, bus, gpa, tLast, tFirst):
-        self.stLast = stLast
-        self.stFirst = stFirst
-        self.grade = grade
-        self.room = room
-        self.bus = bus
-        self.gpa = gpa
-        self.tLast = tLast
-        self.tFirst = tFirst
+        self.stLast = stLast        #[0]
+        self.stFirst = stFirst      #[1]
+        self.grade = grade          #[2]
+        self.room = room            #[3]
+        self.bus = bus              #[4]
+        self.gpa = gpa              #[5]
+        self.tLast = tLast          #[6]
+        self.tFirst = tFirst        #[7]
 
 
-list = []
+def getData(sList):
+    file = open(sys.argv[1], "r")
+    for line in file:
+        line = line.strip().split(",")
+        sList.append(students(line[0],line[1],line[2],line[3],line[4],line[5],line[6],line[7]))
+    return sList
 
+
+
+
+
+
+def main():
+    studentList = []
+    getData(studentList)
+
+    for student in studentList:
+        if student.tFirst == "PERLA":
+            print(student.stFirst + " " + student.stLast)
+
+
+
+if __name__ == "__main__":
+    main()
